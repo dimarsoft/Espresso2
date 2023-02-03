5846d995928602156192caac42e078132f1527a7
Katrin Violet <katea.marandich@gmail.com>
2023-02-03 19:39:26 +0300

    :(

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlite3
+import sys
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QTableWidget, QWidget, QPushButton, \
+    QLabel, QLineEdit, QDialog, QComboBox, QDateTimeEdit
+
+
